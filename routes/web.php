eb125f753b1d8a1765139b682342159fa9f723a1
praditcompsci <praditcompsci@srru.ac.th>
2020-12-03 12:58:57 +0700

    Second code: file upload

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('file-upload', function () {
+    return view('imgupload');
+});
+
+// Route::get('file-upload', function () {
+//     return view('fileupload');
+// });
